11f0ea1a5e3bcea00b36cab18c01fd7095188f23
WisdomNwaiwu <wisdom.nwaiwu.peer@gmail.com>
2025-07-21 10:04:18 +0200

    modified

--- src/Utils/constants.php
+++ src/Utils/constants.php
@@ -1,15 +1,15 @@
-<?php
+# <?php
 
-use Fawaz\config\constants\ConstantsConfig;
+#use Fawaz\config\constants\ConstantsConfig;
 
-if (!function_exists('constants')) {
-    function constants(): ConstantsConfig {
-        static $instance = null;
-
-        if ($instance === null) {
-            $instance = new ConstantsConfig();
-        }
-
-        return $instance;
-    }
-}
+#if (!function_exists('constants')) {
+#    function constants(): ConstantsConfig {
+#       static $instance = null;
+#
+#        if ($instance === null) {
+#            $instance = new ConstantsConfig();
+#        }
+#
+#        return $instance;
+#    }
+#}
